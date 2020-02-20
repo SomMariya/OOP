28648d425f2c528b9e41b44558a3356ab9460573
SomMariya <56650388+SomMariya@users.noreply.github.com>
2020-02-20 22:15:35 +0300

    Add files via upload

---
+++ OOP lab №1(2).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <bitset>
+#include<ctime>
+using namespace std;
+
+bool compare(int, int);
+void incr(int&);
+
+int main()
+{
+	srand(time(NULL));
+	int a = rand() % 10;
+	int b = rand() % 10;
+	bool res = compare(a, b);
+
+	int n = rand() % 10;
+	incr(n);
+}
+
+bool compare(int a, int b)
+{
+	bool res(true);
+
+	if ((a ^ b) == 0) res;
+	else res = false;
+	return res;
+}
+
+void incr(int& n)
+{
+	int n1 = n;
+	for (int i = 0; i < (sizeof(int)*8); i++)
+	{
+		n = n ^ (1 << i);
+		if ((n1 & (1 << i)) == 0) break;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(0, 10);
+            int b = rnd.Next(0, 10);
+            bool res = Compare(a, b);
+
+            int n = rnd.Next(0, 10);
+            Incr(ref n);
+        }
+
+        static bool Compare(int a, int b)
+        {
+            bool res;
+            if ((a ^ b) == 0) res = true;
+            else res = false;
+            return res;
+        }
+
+        static void Incr(ref int n)
+        {
+            int n1 = n;
+            for (int i = 0; i < (sizeof(int)*8); i++)
+            {   
+                n = n ^ (1 << i);
+                if ((n1 & (1 << i)) == 0) break;
+            }
+
+        }
+    }
+}
