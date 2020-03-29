7f504f33043e98100c0577878f914aa57de7b6e6
SomMariya <56650388+SomMariya@users.noreply.github.com>
2020-03-29 21:49:34 +0300

    Add files via upload
    (вар 12)

---
+++ lab3_C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Student students = new Student(3);
+            students[1] = "Jane";
+            students[2] = "Tom";
+            students.output();
+            Console.WriteLine(students.Num);
+
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ lab3_C#/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Student
+    {
+        private int col = 0;
+        private string[] students;
+        public Student(int len)
+        {
+            students = new string[len];
+        }
+
+        public string this[int index]
+        {
+            set
+            {
+                if ((index >= 0) && (index < students.Length))
+                {
+                    students[index] = value;
+                }
+            }
+        }
+        
+        public int Num
+        {
+            get
+            {
+                for (int i = 0; i < students.Length; i++)
+                {
+                    if (students[i] != null)
+                    { col++; }
+                }
+                return col;
+            }
+        }
+
+        public void output()
+        {
+            for (int i = 0; i < students.GetLength(0); i++)
+            {
+                if (students[i] != null)
+                {
+                    Console.Write(students[i]);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+/*
+public string AddStudent()
+{
+    for (int i = 0; i < students.GetLength(0); i++)
+    {
+        if (students[i] == null)
+        {
+            students[i] = (i + 1) + ". " + surname + " " + name + " " + patr;
+            return students[i];
+        }
+    }
+    return null;
+}*/
